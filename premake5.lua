def895b23ab36b65ebfa19142f7c811e4010245e
MayronDAV <118127551+MayronDAV@users.noreply.github.com>
2024-07-11 16:18:20 -0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "GLFW"
+project "glfw"
 	kind "StaticLib"
 	language "C"
 	staticruntime "on"
@@ -10,13 +10,25 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/platform.c",
+		"src/mappings.h",
 		"src/glfw_config.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_joystick.c",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c"
 	}
 
 	filter "system:linux"
@@ -27,16 +39,24 @@ project "GLFW"
 
 		files
 		{
+			"src/posix_time.h",
+			"src/posix_time.c",
+			"src/posix_thread.h",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+
+			"src/x11_platform.c",
+			"src/xkb_unicode.h",
+			"src/xkb_unicode.c",
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
 			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
+
+			"src/linux_joystick.h",
+			"src/linux_joystick.c",
+			"src/posix_poll.h",
+			"src/posix_poll.c",
 		}
 
 		defines
@@ -49,13 +69,22 @@ project "GLFW"
 
 		files
 		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
+
+			"src/win32_time.h",
 			"src/win32_time.c",
+			"src/win32_thread.h",
 			"src/win32_thread.c",
+			"src/win32_module.c",
+
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
+			"src/win32_joystick.c",
+			"src/win32_init.c",
+			"src/win32_monitor.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
+
+			"src/win32_init.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c"
 		}
